696456aa320e1ae02ba413c3ec84ced1cdf320fa
Vardhan <vardhangaonkar079@gmail.com>
2026-03-10 00:04:14 +0530

    Initial Commit

---
+++ src/main/java/com/mycompany/calculatorjavaapp/CalculatorJavaApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculatorjavaapp;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class CalculatorJavaApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
